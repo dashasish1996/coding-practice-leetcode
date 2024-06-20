5801c4a7ecf19494f1875ef381e1e65e39201832
Asish <asishkumardash.1996@gmail.com>
2024-06-20 16:36:11 +0100

    Solved LC-2 Add Two Numbers

--- LC_2_AddTwoNumbers.java
+++ LC_2_AddTwoNumbers.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LC_2_AddTwoNumbers {
 
     /*
@@ -10,7 +13,76 @@ public class LC_2_AddTwoNumbers {
     */
 
     public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
 
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+        System.out.println("l1:");
+        printListNde(l1);
+        System.out.println("l2:");
+        printListNde(l2);
+        System.out.println("Result:");
+        printListNde(addTwoNumbers(l1, l2));
+    }
+
+    public static void printListNde(ListNode v) {
+        List<String> toPrint = new ArrayList<>();
+        while (v != null) {
+            toPrint.add(String.valueOf(v.val));
+            v = v.next;
+        }
+        String p = String.join("->", toPrint);
+        System.out.println(p);
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode rNode = new ListNode();
+        ListNode p = rNode;
+        int carry = 0;
+        while (true) {
+            int sumVal = l1.val + l2.val + carry;
+            p.val = sumVal % 10;
+            carry = sumVal / 10;
+            l1 = l1.next;
+            l2 = l2.next;
+            if (l1 == null || l2 == null)
+                break;
+            p.next = new ListNode();
+            p = p.next;
+        }
+        if (l1 == null)
+            l1 = l2;
+        while (l1 != null) {
+            p.next = new ListNode();
+            p = p.next;
+            int sumVal = l1.val + carry;
+            p.val = sumVal % 10;
+            carry = sumVal / 10;
+            l1 = l1.next;
+        }
+        if (carry > 0)
+            p.next = new ListNode(carry);
+        return rNode;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 
 }
